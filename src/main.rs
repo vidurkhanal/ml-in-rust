e0d8694b74915b9fe4164904877e99c1f729de6f
vidurkhanal <vkhanal@caldwell.edu>
2023-12-26 18:48:50 -0500

    feat: basics of neurons and networks

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod network;
+
+fn main() {
+    println!("Designing Neurons in Rust, may or maybot be a good idea tho. still contempplating")
+}
---
+++ src/network.rs
@@ -0,0 +1,6 @@
+pub struct Network {
+    pub weights: Vec<f64>,
+    pub biases: Vec<f64>,
+    pub layers: Vec<f64>,
+    data: Vec<f64>,
+}
